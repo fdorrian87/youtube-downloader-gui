b817cf02f21d296ed2a6d4f162d7a4c775974ae9
fdorrian87 <22032522+fdorrian87@users.noreply.github.com>
2024-07-18 01:49:49 +0100

    word counter script v2 update

--- wordcounterscript.py
+++ wordcounterscript.py
@@ -1,19 +1,14 @@
-print("Enter the file pathway: ")
-
-try:
-    file_path = input()
+def wordcounter(): # word counter function - opens, reads, counts the words, closes the file
+    file_path = input("Enter the file pathway: ")
     target_file = open(file_path, "r")
     file_content = target_file.read()
     words = file_content.split()
     word_count = len(words)
-    print(word_count)
+    print(f"There are {word_count} words in the file.")
     target_file.close()
+
+try: # basic error handling - can't handle quotation marks yet as it will throw a traceback, but will work on this for v3
+    wordcounter()
 except ValueError:
     print("The file does not exist. Please enter the correct file pathway.")
-    file_path = input()
-    target_file = open(file_path, "r")
-    file_content = target_file.read()
-    words = file_content.split()
-    word_count = len(words)
-    print(word_count)
-    target_file.close()
+    wordcounter()
